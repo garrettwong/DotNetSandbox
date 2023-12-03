05e9f18864f72d37902efefc78a2480649d34c59
Garrett Wong <garrettwong@google.com>
2023-12-03 08:53:24 -0800

    Program.cs

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 internal class Program
 {
@@ -7,18 +8,27 @@ internal class Program
     {
         // Day1Part1.Run(args);
         Day1Part2.Run(args);
-        
-        var x = NotSoSecureCode(args[0]);
 
+        var q = HasNumberEmbedded(args[0]);
+        Console.WriteLine(q);
+        var sqlDataReader = NotSoSecureQuery(args[0]);
+        Console.WriteLine(sqlDataReader);
     }
 
-    public static IDataReader NotSoSecureCode(string sql) {
+    public static IDataReader NotSoSecureQuery(string sql)
+    {
         var connection = new SqlConnection();
         string query = $"SELECT * FROM Users WHERE Name = {sql}";
         var command = new SqlCommand(query, connection);
         connection.Open();
         SqlDataReader reader = command.ExecuteReader();
         return reader;
+    }
+
+    public static Match HasNumberEmbedded(string inputToTest)
+    {
+        const string REGEX = @"^(\d+)+$";
+        return System.Text.RegularExpressions.Regex.Match(inputToTest, REGEX);
     }
 }
 
