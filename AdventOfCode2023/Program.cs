19511e3c15c5ea7996bae67974472a818384aa74
Garrett Wong <garrettwong@google.com>
2023-12-03 00:25:49 -0800

    Add SQL Injection flow

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,9 +1,24 @@
-﻿internal class Program
+﻿using System.Data;
+using System.Data.SqlClient;
+
+internal class Program
 {
     public static void Main(string[] args)
     {
         // Day1Part1.Run(args);
         Day1Part2.Run(args);
+        
+        var x = NotSoSecureCode(args[0]);
+
+    }
+
+    public static IDataReader NotSoSecureCode(string sql) {
+        var connection = new SqlConnection();
+        string query = $"SELECT * FROM Users WHERE Name = {sql}";
+        var command = new SqlCommand(query, connection);
+        connection.Open();
+        SqlDataReader reader = command.ExecuteReader();
+        return reader;
     }
 }
 
